d526354b81d5de2bd8debcdf48b1115e515d8a3a
Yoichi Toyota <wisteria.r.s@gmail.com>
2019-10-22 00:31:40 +0900

    Initial commit.

---
+++ src/__test__/useMediaQuery.test.ts
@@ -0,0 +1,63 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import useMediaQuery from "../useMediaQuery";
+
+const fireResize = (width: number) => {
+  window.innerWidth = width;
+  window.dispatchEvent(new Event('resize'));
+};
+
+const sp = "(max-width: 767px)";
+const pc = "(min-width: 768px)";
+
+jest.useFakeTimers();
+
+describe('useMatchMedia', () => {
+  beforeEach(() => {
+    window.matchMedia = jest.fn().mockImplementation(query => {
+      let matches: boolean = false;
+      if (window.innerWidth <= 767) {
+        matches = query === sp;
+      } else {
+        matches = query === pc;
+      }
+      return { matches: matches }
+    });
+  });
+
+  afterEach(() => {
+    window.matchMedia.mockReset();
+  });
+
+  it('is expected to be defined', () => {
+    expect(useMediaQuery).toBeDefined()
+  });
+
+  it('is expected to sp media query string when innerWidth is 767px', () => {
+    const { result } = renderHook(() => useMediaQuery([sp, pc]));
+    act(() => {
+      fireResize(767);
+    });
+    expect(result.current).toBe(sp);
+  });
+
+  it('is expected to pc meida query string when innerWidth is 768px', () => {
+    const { result } = renderHook(() => useMediaQuery([sp, pc]));
+    act(() => {
+      fireResize(768);
+    });
+    expect(result.current).toBe(pc);
+  });
+
+  it('is expected to change result by firing each resize events', () => {
+    const { result } = renderHook(() => useMediaQuery([sp, pc]));
+    act(() => {
+      fireResize(768);
+    });
+    expect(result.current).toBe(pc);
+    jest.advanceTimersByTime(1000);
+    act(() => {
+      fireResize(767);
+    });
+    expect(result.current).toBe(sp);
+  })
+});
---
+++ src/useMediaQuery.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+
+const useMediaQuery = (mediaQueryList: string[]) => {
+    const [current, setCurrent] = useState();
+
+    useEffect(() => {
+        let mounted: boolean = true;
+        let timeout = null;
+
+        const getCurrentMedia = (mediaList: string[]) => {
+            let result = null;
+            for (const media of mediaList) {
+                if (window.matchMedia(media).matches) {
+                    result = media;
+                    break
+                }
+            }
+            return result
+        };
+
+        const onResize = () => {
+            // 500msに一度しかresizeイベントのcallbackを実行しないようにする
+            if (timeout) return;
+
+            const media: string = getCurrentMedia(mediaQueryList);
+            if (current !== media) {
+                setCurrent(media)
+            }
+
+            timeout = setTimeout(() => timeout = null, 500)
+        };
+
+        setCurrent(getCurrentMedia(mediaQueryList));
+        window.addEventListener('resize', onResize);
+
+        return () => {
+            mounted = false;
+            window.removeEventListener('resize', onResize)
+        }
+    }, [mediaQueryList]);
+
+    return current
+};
+
+export default useMediaQuery;
